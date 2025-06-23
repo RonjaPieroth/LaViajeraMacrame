ed016f63c8583f16e60f6614e13a88ab396511c4
RonjaPieroth <ronja.pieroth@gmail.com>
2025-06-23 16:07:06 +0200

    form works

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -8,12 +8,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactFormComponent {
 
-  contactForm = new FormGroup({
+  contactForm: FormGroup = new FormGroup({
   name: new FormControl("",Validators.required),
   email: new FormControl("",[Validators.required, Validators.email]),
   phone: new FormControl(),
   requestType: new FormControl("", Validators.required),
   message: new FormControl("",Validators.required)
   })
+
+  onSubmit(){
+    if (this.contactForm.valid){
+      const form = document.createElement('form');
+      form.action = 'https://formsubmit.co/laviajeramacrame@gmail.com';
+      form.method = 'POST';
+
+      for (const key of Object.keys(this.contactForm.value)) {
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = key;
+        input.value = this.contactForm.value[key];
+        form.appendChild(input);
+      }
+
+      document.body.appendChild(form);
+      form.submit();
+    }
+  }
 
 }
